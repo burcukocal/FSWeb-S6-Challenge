d98d306af6f6edd961e855c89a88264bcc0e3603
burcukocal~ <burcukocal97@gmail.comgit config --global user.name burcukocal~git config --global user.email burcukocal97@gmail.com>
2023-06-02 20:39:23 +0300

    Karakterler eklendi

--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,16 @@
 // Karakter bileşeniniz buraya gelecek
+const Karakter = (props) => {
+    //const data = props.data;
+ 
+
+
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Karakter;
